dd9829e0ebe0b7965c6d5ec869d7f629a1fad578
prospermbuma <prospermbuma@gmail.com>
2025-05-23 02:46:11 +0300

    Updates...

--- main.go
+++ main.go
@@ -7,8 +7,25 @@ import (
 func main() {
 	fmt.Println("Hello Go World")
 	name := "Prosper"
+	var email string = "prospermbuma@gmail.com"
+	var age int = 30
 	greetings(name)
-	for i := 1; i <= 10; i++ {
+
+	fmt.Println()
+	fmt.Println("======== User Profile ========")
+	fmt.Println("Name:", name)
+	fmt.Println("Email:", email)
+	fmt.Println("Age:", age)
+	fmt.Println("==============================")
+
+	fmt.Println()
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for _, num := range numbers {
+		fmt.Println(num)
+	}
+
+	fmt.Println()
+	for i := 1; i <= 5; i++ {
 		fmt.Println("i =", 100/i)
 	}
 }
