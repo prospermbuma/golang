4de38c876b64f0939d323a42d40d47f26de927be
prospermbuma <prospermbuma@gmail.com>
2025-05-23 02:30:05 +0300

    Added a greetings function and for loop

--- main.go
+++ main.go
@@ -6,4 +6,13 @@ import (
 
 func main() {
 	fmt.Println("Hello Go World")
+	name := "Prosper"
+	greetings(name)
+	for i := 1; i <= 10; i++ {
+		fmt.Println("i =", 100/i)
+	}
+}
+
+func greetings(name string) {
+	fmt.Println("Hello " + name)
 }
