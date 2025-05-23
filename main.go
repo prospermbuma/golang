e689a5029eb95e3c64f2bc27b699d10ce03f70f0
prospermbuma <prospermbuma@gmail.com>
2025-05-23 17:13:11 +0300

    Updating..

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 )
 
+// GLOBAL VARIABLES
+var name string = "Prosper"
+var email string = "prospermbuma@gmail.com"
+var age int = 30
+
+// MAIN FUNCTION
 func main() {
 	fmt.Println("Hello Go World")
-	name := "Prosper"
-	var email string = "prospermbuma@gmail.com"
-	var age int = 30
 	greetings(name)
 
 	fmt.Println()
@@ -19,11 +22,23 @@ func main() {
 	fmt.Println("==============================")
 
 	fmt.Println()
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// ARRAY - Is the list with a fixed size, for example 10 numbers.
+	// Loop through an array
+	numbers := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	for _, num := range numbers {
 		if num%2 == 0 && num > 2 && num < 10 {
 			fmt.Println(num)
 		}
+	}
+
+	fmt.Println()
+	// SLICE - Is the list with NO fixed size
+	// Loop through a slice
+	names := []string{"Victoria", "Prosper", "Joseph", "Gladness"}
+	for index, name := range names {
+		index++
+		//fmt.Print(index, ". ", name, "\n")
+		fmt.Printf("%d. %s\n", index, name)
 	}
 
 	fmt.Println()
@@ -32,6 +47,8 @@ func main() {
 	}
 }
 
+// FUNCTION DECLARATION
+// User-defined functions
 func greetings(name string) {
 	fmt.Println("Hello " + name)
 }
